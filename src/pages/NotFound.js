cf21ad423397c3b5856ef49d2eb674553a7b47ae
Battula Reddy <battulareddy@Battulas-MacBook-Pro.local>
2020-07-13 10:32:39 +0530

    react website modified

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
  */
 const NotFound = () => (
   <div className="not-found">
-    <h1>404</h1>
-    <h2>Document not found</h2>
-    <p><Link to="/">Return to homepage</Link></p>
+    <h1>Nishanth Reddy B</h1>
+    <h2>nishanthreddyb0@gmail.com</h2>
+    <p><Link to="/Preview">Preview</Link></p>
   </div>
 );
 
